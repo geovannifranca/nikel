3f17f1a89b39920d884f1315d79a355071df5cb1
geovannifranca <geovannidefranca98@gmail.com>
2025-11-23 21:15:37 -0300

    finalizado

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,36 +1,100 @@
-
 const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
 
+checkLogged();
+
+//logar no sistema
+
+document.getElementById("login-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email-input").value;
+  const password = document.getElementById("password-input").value;
+  const checkSession = document.getElementById("session-check").checked;
+
+  const account = getAccount(email);
+
+  if (!account) {
+    alert("Opps, verifique o usuário ou a senha.");
+    return;
+  }
+
+  if (account) {
+    if (account.password !== password) {
+      alert("Opps, verifique o usuário ou a senha.");
+      return;
+    }
+
+    saveSession(email,checkSession);
+
+    window.location.href = "home.html";
+  }
+
+  console.log(email, session, password);
+});
 
 //criar conta
 
-document.getElementById("create-form").addEventListener("submit", function(e){
-    e.preventDefault();
+document.getElementById("create-form").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const  email = document.getElementById("email-create-input").value;
-    const password = document.getElementById("password-create-input").value;
+  const email = document.getElementById("email-create-input").value;
+  const password = document.getElementById("password-create-input").value;
 
-    if(email.length < 5){
-        alert("Preencha o campo com e-mail valido.");
-        return;
-    }
+  if (email.length < 5) {
+    alert("Preencha o campo com e-mail valido.");
+    return;
+  }
 
-    if(password.length < 4){
-        alert("Preencha a senho com no mínimo 4 dígito");
-        return;
-    }
+  if (password.length < 4) {
+    alert("Preencha a senho com no mínimo 4 dígito");
+    return;
+  }
 
-    saveAccount({
-        login: email,
-        password: password,
-        transactions: [],
-    });
+  saveAccount({
+    login: email,
+    password: password,
+    transactions: [],
+  });
 
-    myModal.hide();
+  myModal.hide();
 
-    alert("Conta criada com sucesso.")
+  alert("Conta criada com sucesso.");
 });
 
-function saveAccount(data){
-    localStorage.setItem(data.login, JSON.stringify(data));
+
+function checkLogged(){
+    if(session){
+        sessionStorage.getItem("logged",session);
+        logged = session;
+    }
+
+    if(logged){
+        saveSession(logged,session);
+        window.location.href = "home.html";
+    }
 }
+
+function saveSession(data,saveSesseion){
+    if(saveSesseion){
+        localStorage.setItem("session",data);
+    }
+
+    sessionStorage.setItem("logged", data);
+}
+
+function saveAccount(data) {
+  localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function getAccount(key) {
+  const account = localStorage.getItem(key);
+
+  if (account) {
+    return JSON.parse(account);
+  }
+
+  return "";
+}
+
